e14ec27c8cdeebed648a9f52ea2d0dea7a7a0d55
Darren Shepherd <darren.s.shepherd@gmail.com>
2013-10-24 00:19:04 -0700

    Random changes

--- launcher/src/main/java/io/github/gumbo/launcher/jetty/Main.java
+++ launcher/src/main/java/io/github/gumbo/launcher/jetty/Main.java
@@ -5,13 +5,14 @@ import java.net.URL;
 import java.util.TimeZone;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 
 public class Main {
 
 	public static final String WEB_XML = "/WEB-INF/web.xml";
-	
+
 	public static final String[] WEB_XML_PATHS = new String[] {
 			"app/src/main/webapp/WEB-INF/web.xml",
 			"src/main/webapp/WEB-INF/web.xml" };
@@ -30,33 +31,34 @@ public class Main {
 	public static void main(String... args) {
 		/* The world is better place without time zones.  Well, at least for computers */
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		
+
 		long start = System.currentTimeMillis();
-		
-		try {			
+
+		try {
 			Server s = new Server(8080);
-	
+
 			WebAppContext context = new WebAppContext();
 			context.setThrowUnavailableOnStartupException(true);
-			
+
 			File webXmlFile = getWebXml();
-			
-			URL webXml = webXmlFile == null ? Main.class.getResource(WEB_XML) : webXmlFile.toURI().toURL(); 
-			URL contextRoot = webXmlFile == null ? Main.class.getResource("") : 
+
+			URL webXml = webXmlFile == null ? Main.class.getResource(WEB_XML) : webXmlFile.toURI().toURL();
+			URL contextRoot = webXmlFile == null ? Main.class.getResource("") :
 				webXmlFile.getParentFile().getParentFile().toURI().toURL();
-			
+
 			if ( webXml != null ) {
 				context.setDescriptor(webXml.toExternalForm());
 			}
-			
+
 			if ( contextRoot != null ) {
 				context.setWar(contextRoot.toExternalForm());
 			}
-			
-			context.setClassLoader(Main.class.getClassLoader());
-			
+
+			context.setClassLoader(new WebAppClassLoader(Main.class.getClassLoader(), context));
+			context.setContextPath("/client");
+
 			s.setHandler(context);
-		
+
 			s.start();
 			s.join();
 		} catch (Exception e) {
